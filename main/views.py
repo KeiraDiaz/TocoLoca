f91db8751887728656cad12894fbc8cd63e42ff7
KeiraDiaz <nkeiradiaz@gmail.com>
2024-10-08 00:20:40 +0700

    Assignment 6, but only coding, readme tba

--- main/views.py
+++ main/views.py
@@ -12,22 +12,22 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 @login_required(login_url='/login')
 def show_main(request):
-    item_entries = ItemEntry.objects.filter(user=request.user)
 
     context = {
         'Name' : request.user.username,
         'NPM' : "2306256394",
         'Class': 'KKI',
-        'item_entries' : item_entries,
         'last_login': request.COOKIES.get('last_login',"Not set"),
     }
 
     return render(request, "main.html", context)
 
-def create_new_item(request):
+def create_item_entry(request):
     form = ItemEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
@@ -37,14 +37,14 @@ def create_new_item(request):
         return redirect('main:show_main')
 
     context = {'form': form}
-    return render(request, "create_new_item.html", context)
+    return render(request, "create_item_entry.html", context)
 
 def show_xml(request):
-    data = ItemEntry.objects.all()
+    data = ItemEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = ItemEntry.objects.all()
+    data = ItemEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -68,20 +68,22 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
+    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
 
       if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+      else:
+        messages.error(request, "Invalid username or password. Please try again.")
 
-   else:
+    else:
       form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -90,14 +92,10 @@ def logout_user(request):
     return response
 
 def edit_item(request, id):
-    # Get mood entry based on id
     item = ItemEntry.objects.get(pk = id)
-
-    # Set mood entry as an instance of the form
     form = ItemEntryForm(request.POST or None, instance=item)
 
     if form.is_valid() and request.method == "POST":
-        # Save form and return to home page
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
@@ -105,8 +103,25 @@ def edit_item(request, id):
     return render(request, "edit_item.html", context)
 
 def delete_item(request, id):
-    # Get mood based on id
     item = ItemEntry.objects.get(pk = id)
     item.delete()
-    # Return to home page
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_item_entry_ajax(request):
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        desc = request.POST.get("desc")
+        user = request.user
+
+    
+        new_item = ItemEntry(
+            name=name, 
+            price=price,
+            desc=desc,
+            user=user
+        )
+        new_item.save()
+
+        return HttpResponse(b"CREATED", status=201)
